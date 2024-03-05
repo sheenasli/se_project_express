7a865b509327cbf00c48c53ac6c5e163e5637f36
Sheena Irvin <sheenu.sli@gmail.com>
2024-03-04 18:15:43 -0600

    update ReadMe info

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ mongoose.connect(
 app.use(helmet());
 app.use(express.json());
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use(requestLogger);
 app.use(routes);
 
